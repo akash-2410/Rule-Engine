34c7d55e9fe0d47510bf70cbb28ae72a093c8d16
Akash-2009 <pandeyakashar098@gmail.com>
2024-10-18 11:59:12 +0530

    first commit

---
+++ backend/main.py
@@ -0,0 +1,70 @@
+
+
+import argparse
+import unittest
+import uvicorn
+import tests.test_parser
+import tests.test_tree_traversal
+
+def _run_tests():
+    """Run tests."""
+    print("--test: running tests")
+
+    loader = unittest.TestLoader()
+    suite_parser = loader.loadTestsFromModule(tests.test_parser)
+    suite_tree = loader.loadTestsFromModule(tests.test_tree_traversal)
+
+    runner = unittest.TextTestRunner()
+    runner.run(suite_parser)
+    runner.run(suite_tree)
+
+def _run_dev_api_server(host = None, port = None):
+
+    if not host:
+        host = "0.0.0.0"
+
+    if not port:
+        port = 5000
+
+    uvicorn.run('rule_engine.main:app', host=host, port=port, reload=True)
+
+def _run_db_migrate():
+    print("--migrate: running DB Migrate")
+
+def _run_start_db():
+   
+    print("--db: starting DB")
+
+def _show_help():
+    help_string = (
+        "usage: main.py [-h] [--tests] [--dev] [--host HOST] [--port PORT]\n\n"
+        "options:\n"
+        "-h, --help         show this help message and exit\n"
+        "--tests            Run tests for Parser and AST\n"
+        "--dev              Run dev FastAPI Server\n"
+        "--host HOST        Add host address to run the FastAPI Server\n"
+        "--port PORT        Add port address to run the FastAPI Server\n"
+    )
+
+    print(help_string)
+
+def main():
+    
+    parser = argparse.ArgumentParser(allow_abbrev=False)
+
+    parser.add_argument('--tests', action='store_true', help='Run tests for Parser and AST')
+    parser.add_argument('--dev', action='store_true', help='Run dev FastAPI Server')
+    parser.add_argument('--host', dest='host', type=str, help='Add host address to run the FastAPI Server')
+    parser.add_argument('--port', dest='port', type=int, help='Add port address to run the FastAPI Server')
+
+    args = parser.parse_args()
+
+    if args.tests:
+        _run_tests()
+    elif args.dev:
+        _run_dev_api_server(args.host, args.port)
+    else:
+        _show_help()
+
+if __name__ == "__main__":
+    main()
---
+++ backend/rule_engine/models.py
@@ -0,0 +1,30 @@
+
+
+import os
+from dotenv import load_dotenv
+from sqlalchemy import Column, Integer, String, Text, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+load_dotenv()
+
+DATABASE_URL = (
+    f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
+    f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+)
+
+Base = declarative_base()
+
+class Rule(Base):
+   
+    __tablename__ = "rules"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    ast_json = Column(Text, nullable=False)
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base.metadata.create_all(bind=engine)
